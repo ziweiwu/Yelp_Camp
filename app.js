7cc46653f65819ab0a1845912257f51f4d2931a5
ziwei wu <ziweiwu@gmail.com>
2017-12-28 19:07:26 -0500

    fixed app.js

--- app.js
+++ app.js
@@ -57,4 +57,4 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen((process.env.PORT, process.env.IP);
+app.listen(process.env.PORT, process.env.IP);
