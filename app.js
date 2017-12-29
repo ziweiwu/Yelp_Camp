09788dd567f2d5806bfaf0cdbbb8ad5965a5bba0
ziwei wu <ziweiwu@gmail.com>
2017-12-28 19:06:14 -0500

    fixed app.js

--- app.js
+++ app.js
@@ -57,6 +57,4 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(3000, function(){
-   console.log("The YelpCamp Server Has Started!");
-});
+app.listen((process.env.PORT, process.env.IP);
